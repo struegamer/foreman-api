84fbac050fb1e7020ef4bb1c685bd0ac0366f76e
Stephan Adig <sh@sourcecode.de>
2012-10-26 21:54:13 +0200

    updated

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='python-foreman-api',
+      version='0.1',
+      author='Stephan Adig',
+      author_email='sh@sourcecode.de',
+      description='A Foreman REST API Python Wrapper Library',
+      license='LGPLv2', url='https://github.com/sadig/foreman-api',
+      packages=[
+        'foreman',
+        'foreman.api'],
+      install_requires=['restkit'],
+      scripts=['scripts/foreman-cli.py']
+      )
